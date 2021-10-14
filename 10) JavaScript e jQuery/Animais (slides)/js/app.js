7d5f0917f2aa45f9259fbdeb4119305c6d1d54f7
Lucas Apolinário <lucas.apolinario98@gmail.com>
2021-10-14 11:45:38 -0300

    curso de jQuery & JavaScript finalizado!

--- 10) JavaScript e jQuery/Animais (slides)/js/app.js
+++ 10) JavaScript e jQuery/Animais (slides)/js/app.js
@@ -1,3 +1,24 @@
+// DEBOUNCE DO LODASH
+
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
+
+// MUDAR TAB AO CLICK
+
 $('[data-group]').each(function(){
 	var $allTarget = $(this).find('[data-target]'),
 			$allClick = $(this).find('[data-click]'),
@@ -25,25 +46,22 @@ $('[data-group]').each(function(){
 // SCROLL SUAVE
 
 // [href^="#"] = seleciona todos os elementos (menus internos) que possuem um href começando com '#', deixando o preventDefault somente nesses
-$('.menu-nav a[href^="#"]').click(function(e) {
+$('.menu-nav a[href^="#"]').click(function(e){
 	e.preventDefault();
-
 	var id = $(this).attr('href'),
-		menuHeight = $('.menu').innerHeight(),
-		targetOffset = $(id).offset().top;
-
+			menuHeight = $('.menu').innerHeight(),
+			targetOffset = $(id).offset().top;
 	$('html, body').animate({
 		scrollTop: targetOffset - menuHeight
-	}, 500)
-})
+	}, 500);
+});
 
-$('.logo').click(function(e) {
+$('.logo').click(function(e){
 	e.preventDefault();
-
 	$('html, body').animate({
 		scrollTop: 0
 	}, 500)
-})
+});
 
 
 
@@ -55,24 +73,42 @@ $('section').each(function() {
 	var height = $(this).height(),
 		// diferença entre o elemento da section com o topo
 		offsetTop = $(this).offset().top,
+		menuHeight = $('.menu').innerHeight(),
 		// pega o nome dos ids das sections
 		id = $(this).attr('id'),
 		// pega o objeto com o nome com o id da section
-		$itemMenu = $('a[href="#' + id + '"]'),
-		menuHeight = $('.menu').innerHeight();
+		$itemMenu = $('a[href="#' + id + '"]');
 	
 	// verifica se está ou não na section
-	$(window).scroll(function() {
+	$(window).scroll(debounce(function(){
 		var scrollTop = $(window).scrollTop();
 		
 		// caso o offsetTop é menor que o scrollTop && se caso o offsetTop + height for maior que scrollTop
-		if (offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
-			$itemMenu.addClass('active')
+		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+			$itemMenu.addClass('active');
 		} else {
-			$itemMenu.removeClass('active')
+			$itemMenu.removeClass('active');
 		}
-	})
-})
+	}, 200));
+});
+
+
+$('section').each(function(){
+	var height = $(this).height(),
+			offsetTop = $(this).offset().top,
+			menuHeight = $('.menu').innerHeight(),
+			id = $(this).attr('id'),
+			$itemMenu = $('a[href="#' + id + '"]');
+	
+	$(window).scroll(function(){
+		var scrollTop = $(window).scrollTop();
+		if(offsetTop - menuHeight < scrollTop && offsetTop + height - menuHeight > scrollTop) {
+			$itemMenu.addClass('active');
+		} else {
+			$itemMenu.removeClass('active');
+		}
+	});
+});
 
 
 
@@ -80,12 +116,72 @@ $('section').each(function() {
 
 $('.mobile-btn').click(function(){
 	$(this).toggleClass('active');
-	$('.mobile-menu').toggleClass('active')
-})
+	$('.mobile-menu').toggleClass('active');
+});
 
 
 
 // SLIDES
 
-// .slide > :first = vai pegar o primeiro item que está dentro de slide
-$('.slide > :first').addClass('active')
+(function() {
+	function slider(sliderName, velocidade) {
+		var sliderClass = '.' + sliderName,
+				activeClass = 'active',
+				rotate = setInterval(rotateSlide, velocidade);
+		
+		// .slide > :first = vai pegar o primeiro item que está dentro de slide
+		$(sliderClass + ' > :first').addClass(activeClass);
+
+		$(sliderClass).hover(function(){
+			clearInterval(rotate);
+		}, function() {
+			rotate = setInterval(rotateSlide, velocidade);
+		});
+		
+		function rotateSlide() {
+			var activeSlide = $(sliderClass + ' > .' + activeClass),
+				// next = passa para a próxima div
+				nextSlide = activeSlide.next();
+
+			// caso o length do próximo slide for igual a zero...
+			if(nextSlide.length == 0) {
+				nextSlide = $(sliderClass + ' > :first');
+			}
+			activeSlide.removeClass(activeClass);
+			nextSlide.addClass(activeClass);
+		} 
+	}
+
+	slider('introducao', 2000);
+})();
+
+
+// ANIMAÇÃO AO SCROLL
+
+(function() {
+	var $target = $('[data-anime="scroll"]'),
+	animationClass = 'animate',
+	// (window).height() * 3/4 = altura total da janela vezes 3/4
+	offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+		
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+
+			// se a distância do topo até o elemento for maior que...
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 100));
+})();
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ORIGAMID_CURSOS
-Estudos/Anotações dos cursos do Origamid.
+<h1 align="left"> Cursos Origamid 🐺</h1>
+<h2 align="left">
+    <img alt="Origamid" src="bg-origamid.png" width="700px" />
+    <p style="font-size: 14px;">Repositório sobre os estudos no curso da origamid | Aluno: Lucas Gomes</p>
+</h2>
+
+<p>Legenda: ✔️ concluido, ⬜ falta concluir, 🚧 em andamento, 🛑 em pausa, 🏆 certificado,  📂 projeto </p>
+
+<p>🚧 Web Design Completo <a target="_blank" href="https://www.origamid.com/"></a></p>
+<p>⬜ CSS Avançado Posicionamento</p>
+<p>⬜ CSS Flexbox <a target="_blank" href="https://www.origamid.com"</a> <a target="_blank" href="https://github.com/lulinux21"></a> </p>
+<p>⬜ JavaScript Completo ES6</p>
+<p>⬜ CSS com SASS</p>
+<p>⬜ React Completo</p>
+<p>⬜ Redux com React</p>
+<p>⬜ CSS Grid Layout</p>
+<p>⬜ WordPress Como CMS</p> 
+<p>⬜ Wordpress REST API</p>
+<p>⬜ Vue.js Completo</p>
+<p>⬜ WooCommerce Como CMS</p>
+<p>⬜ Bootstrap 4</p>
+<p>⬜ Automação Front End com NPM</p>
+<p>⬜ JavaScript e jQuery </p>
+<p>⬜ UI Design Avançado</p>
+<p>⬜ UX Design Heurísticas</p>
+<p>⬜ Tipografia Avançada</p>
+<p>⬜ Adobe XD</p>
